c20fb8bc3bbccf56324cac2ee62e3c221b0a2d93
Latfiansya <latfiansyadzikri@gmail.com>
2024-05-22 00:20:38 +0700

    Membuat prosedur untuk tambah dokter pada class dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -37,6 +37,7 @@ public:
 void pasien::tambahDokter(dokter* pDokter) {
     daftar_dokter.push_back(pDokter);
 }
+
 void pasien::cetakDokter() {
     cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
     for (auto& a : daftar_dokter) {

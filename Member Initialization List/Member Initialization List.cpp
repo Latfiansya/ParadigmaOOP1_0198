49ddc1bdf152f03542cde4cd12301a3c66741438
Latfiansya <latfiansyadzikri@gmail.com>
2024-05-22 00:10:00 +0700

    Membuat header anak

---
+++ Member Initialization List/Member Initialization List.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    Mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {//member initialization list
+        //definisi}
+    }
+    ~Mahasiswa() {
+        cout << "Id = " << id << endl;
+        cout << "nama = " << nama << endl;
+        cout << "nilai = " << nilai << endl;
+
+    }
+};
+
+
+int main()
+{
+    Mahasiswa mhs(12, "asroni", 90.5);
+
+    return 0;
+
+}
